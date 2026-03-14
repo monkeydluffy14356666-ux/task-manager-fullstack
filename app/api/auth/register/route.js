c50b63b8e5c057f1de22070a52c7a7e4ac277741
Rachith Kumar <rachithkumar@email.com>
2026-03-14 22:04:15 +0530

    fix register api

---
+++ app/api/auth/register/route.js
@@ -0,0 +1,52 @@
+import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import connectDB from "@/lib/db";
+import User from "@/models/User";
+
+export async function POST(req) {
+  try {
+
+    await connectDB();
+
+    const { name, email, password } = await req.json();
+
+    if (!name || !email || !password) {
+      return NextResponse.json(
+        { message: "All fields required" },
+        { status: 400 }
+      );
+    }
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { message: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await User.create({
+      name,
+      email,
+      password: hashedPassword
+    });
+
+    return NextResponse.json(
+      { message: "User registered successfully" },
+      { status: 201 }
+    );
+
+  } catch (error) {
+
+    console.log(error);
+
+    return NextResponse.json(
+      { message: "Server error" },
+      { status: 500 }
+    );
+
+  }
+}
